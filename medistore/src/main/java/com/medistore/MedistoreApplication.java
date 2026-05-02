5b73dc04ed811bd907f9ecbf8c406928d51d2f3e
IT25102567 <IT25102567@my.sliit.lk>
2026-05-02 13:07:21 +0530

    Feedback & Review Management

---
+++ medistore/src/main/java/com/medistore/MedistoreApplication.java
@@ -0,0 +1,13 @@
+package com.medistore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedistoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedistoreApplication.class, args);
+	}
+
+}
---
+++ medistore/src/test/java/com/medistore/MedistoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medistore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedistoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
