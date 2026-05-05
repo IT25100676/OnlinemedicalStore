f34bf9fc77dc42fae0728345f38721e69e4fe465
IT25102567 <IT25102567@my.sliit.lk>
2026-05-05 12:06:29 +0530

    Update MedistoreApplication.java
    Added Javadoc comments to describe the application and its purpose.

--- medistore/src/main/java/com/medistore/MedistoreApplication.java
+++ medistore/src/main/java/com/medistore/MedistoreApplication.java
@@ -3,6 +3,13 @@ package com.medistore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * MediStore Online Medical Store Management System
+ * Component 01: User Management
+ *
+ * Entry point for the Spring Boot application.
+ */
+
 @SpringBootApplication
 public class MedistoreApplication {
 
